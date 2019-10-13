25f1aba727e18f2e11c65b7632a9f181a67dc7b7
Emery Tate <tateejt@gmail.com>
2019-10-13 20:49:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,4 +12,8 @@ def line(array)
   end
 end
 
+def take_a_number(line, new_person)
+  line.push(new_person) # could say: "line << new_person"
+  puts "Welcome, #{new_person}. You are number #{line.length} in line."
+end
 
